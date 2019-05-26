2b096aefa3f39c0ed18a86346cfedfe4e82168f1
skyepodium <skyepodium@gmail.com>
2019-05-26 19:35:39 +0900

    initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const http = require('http')
+const socket = require('socket.io')
+
+let server = http.createServer((req, res) => {
+}).listen(80);
+
+const io = socket(server)
+
+io.on('connection', (socket) => {
+    socket.on('chat', (msg) => {
+        let data = JSON.parse(msg)
+        console.log(data)
+        console.log(data.name)
+        console.log(data.message)
+        io.emit('chat', data)
+    })
+})
